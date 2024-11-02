1b35d9c5a4350cea509b13451fdc2fa746cd7ed5
Alexander <158212963+SashaKhmur03@users.noreply.github.com>
2024-11-02 20:21:41 +0300

    Update СustomUserDetails.java
    update СustomUserDetails

--- demo-boot/src/main/java/by/itstep/sasha/demoboot/dto/UserDetails.java
+++ demo-boot/src/main/java/by/itstep/sasha/demoboot/dto/UserDetails.java
@@ -1,12 +1,19 @@
 package by.itstep.sasha.demoboot.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
 
-public class UserDetails {
+@AllArgsConstructor
+@Data
+public class CustomUserDetails implements UserDetails {
     private String firstName;
     private String lastName;
+    private String username;
+    private String password;
     private Collection<? extends GrantedAuthority> authorities;
     private String email;
     private String phoneNumber;
