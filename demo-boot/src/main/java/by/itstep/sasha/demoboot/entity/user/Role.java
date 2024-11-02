4501dcc2752d1615fb743bbb257e6912b9c71d36
Alexander <158212963+SashaKhmur03@users.noreply.github.com>
2024-11-02 20:48:16 +0300

    Add files via upload

---
+++ demo-boot/src/main/java/by/itstep/sasha/demoboot/entity/user/Role.java
@@ -0,0 +1,26 @@
+package by.itstep.sasha.demoboot.entity.user;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Set;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Data
+@Table(name = "roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_role", unique = true, nullable = false)
+    private int id;
+
+    @Column(name = "name",  length = 128, nullable = false)
+    private String name;
+
+    @OneToMany(mappedBy = "role")
+    private Set<User> users;
+}
