0f459de843dfe5dbfe2f2c95f1781291a51605ce
Alexander <158212963+SashaKhmur03@users.noreply.github.com>
2024-11-02 20:44:32 +0300

    Delete demo-boot/src/main/java/by/itstep/sasha/demoboot/dao/interfacedao/userdao/RoleDao.java
    Delete RoleDao

--- demo-boot/src/main/java/by/itstep/sasha/demoboot/dao/interfacedao/userdao/RoleDao.java
+++
@@ -1,8 +0,0 @@
-package by.itstep.sasha.demoboot.dao.interfacedao.userdao;
-
-import by.itstep.sasha.demoboot.entity.user.User;
-
-public interface UserDao {
-
-    User findByEmail(String email);
-}
